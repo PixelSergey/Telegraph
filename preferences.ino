36aca4537966e13e5f1a240ae4d5c74b8970e399
PixelSergey <sergey.ichtchenko@gmail.com>
2020-02-02 12:25:34 +0200

    Add base files

---
+++ preferences.ino
@@ -0,0 +1,34 @@
+/**    
+ *  Telegraph's configuration file. This file is a prototype; it must be
+ *  edited before compilation to set up various constants the program uses.
+ *  Copyright (C) 2020 PixelSergey
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef TELEGRAPH_PREF
+#define TELEGRAPH_PREF
+
+#include <ThingerESP8266.h>
+
+// NodeMCU preferences
+#define WIFI_SSID "WiFi SSID"
+#define WIFI_PASSWORD "WiFi Password"
+
+// Thinger.io preferences
+#define USERNAME "Username"
+#define DEVICE_ID "Device ID"
+#define DEVICE_CREDENTIAL "Device Credential"
+
+#endif
---
+++ telegraph.ino
@@ -0,0 +1,33 @@
+/**    
+ *  Telegraph's main program file.
+ *  Copyright (C) 2020 PixelSergey
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/** Configuration preferences are defined in the preferences.ino file and are
+ *  automatically included by the Arduino compiler.
+ */
+
+// Pin definitions
+#define BUTTON D3
+#define SPEAKER D5
+
+void setup(){
+    
+}
+
+void loop(){
+    
+}
