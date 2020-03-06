4b9b182a5dd760b318cb64668d1cbbecaa2cc1d1
PixelSergey <sergey.ichtchenko@gmail.com>
2020-03-06 19:27:19 +0200

    Add networking and streaming to the telegraph

--- telegraph.ino
+++ telegraph.ino
@@ -20,26 +20,56 @@
  * The preferences file is automatically included here by the Arduino IDE.
  */
 
+int start_time;
+int current_time;
+bool stream;
+
+ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
+
 void setup(){
     pinMode(BUTTON, INPUT_PULLUP);
     pinMode(SPEAKER, OUTPUT);
     pinMode(LED_RED, OUTPUT);
     pinMode(LED_GRN, OUTPUT);
     pinMode(LED_BLU, OUTPUT);
+    
+    start_time = millis();
+    current_time = millis();
+    stream = false;
+
+    thing.add_wifi(WIFI_SSID, WIFI_PASSWORD);
+
+    thing["button"] >> [](pson& out){
+    out = digitalRead(BUTTON);
+  };
 }
 
 void loop(){
-    int state = digitalRead(BUTTON);
-    if(state == LOW){  // The button is pressed; state inverted due to pullup pin
+    thing.handle();
+    int button = digitalRead(BUTTON);
+    if(button == LOW){  // The button is pressed; state inverted due to pullup pin
         tone(SPEAKER, PITCH);
         // Due to the RGB led being common anode, 0 turns it fully on and 1023 turns it fully off
         analogWrite(LED_GRN, 0);
         analogWrite(LED_RED, 1023);
-        analogWrite(LED_BLU, 1023);
+
+        current_time = millis();
+        if(current_time - start_time >= 2500){
+            stream = !stream;
+            start_time = millis();
+        }
     }else{
         noTone(SPEAKER);
         analogWrite(LED_GRN, 1023);
         analogWrite(LED_RED, 1023);
+
+        start_time = millis();
+    }
+
+    if(stream){
+        thing.stream(thing["button"]);
+        analogWrite(LED_BLU, 0);
+    }else{
         analogWrite(LED_BLU, 1023);
     }
 }
