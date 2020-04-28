b360c61178adffb11b2c77128cd0658a898aa722
PixelSergey <sergey.ichtchenko@gmail.com>
2020-04-29 00:38:31 +0300

    Greatly simplify and refactor code
    I realised that I could change approaches to the problem entirely.
    The current approach requires me to manually detect when the button
    gets pressed and released, along with a streaming/receiving mode.
    The current approach also tries to constantly update the receiver's
    data in a way similar to streaming. The ESP8266 module is not fast
    enough for this.
    This approach uses a lot of resources and variables, and is generally
    confusing.
    
    The new approach uses Arduino's interrupt feature. This automatically
    sends an update to the receiver whenever the BUTTON pin's state changes.
    Streaming/receiving mode can be re-added later if needed.

--- telegraph.ino
+++ telegraph.ino
@@ -20,96 +20,49 @@
  * The preferences file is automatically included here by the Arduino IDE.
  */
 
-int start_time;
-int current_time;
-int recvstate;
-bool stream;
-bool recv;
-char recvname[9];
-
 ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
+char recvname[9]; // The name of the receiver
+
+void send_state(int state){
+    pson data;
+    data["state"] = state;
+    thing.call_device(recvname, "state", data);
+}
+
+// These two functions call the send_state function separately due to the fact that a function that is
+// attached as a callback cannot take any parametres and cannot return anything.
+void send_on(){send_state(1);}
+void send_off(){send_state(0);}
 
 void setup(){    
     pinMode(BUTTON, INPUT_PULLUP);
     pinMode(SPEAKER, OUTPUT);
+    // Due to the RGB led being common anode, 0 turns an LED fully on and 1023 turns it fully off
     pinMode(LED_RED, OUTPUT);
     pinMode(LED_GRN, OUTPUT);
     pinMode(LED_BLU, OUTPUT);
-    
-    start_time = millis();
-    current_time = millis();
-    stream = false;
-    recv = false;
+
+    attachInterrupt(BUTTON, send_on, FALLING);
+    attachInterrupt(BUTTON, send_off, RISING);
 
     // The most cursed and hacky line of code you will ever see
     // Converts NodeMCU1 <-> NodeMCU0 to find out the recipient's name
     sprintf(recvname, "NodeMCU%d", !((int)DEVICE_ID[7]-48));
 
     thing.add_wifi(WIFI_SSID, WIFI_PASSWORD);
-
-    thing["button"] >> [](pson& out){
-        out = digitalRead(BUTTON);
-    };
-    thing["listener"] << [](pson& in){
-        recvstate = in;
-    };
-    thing["torecv"] = [](){
-        recv = !recv;
+    
+    thing["state"] << [](pson& in){
+        int command = (int) in["state"];
+        if(command == 1){
+            analogWrite(LED_GRN, 0);
+            tone(SPEAKER, PITCH);
+        }else{
+            analogWrite(LED_GRN, 0);
+            noTone(SPEAKER);
+        }
     };
 }
 
 void loop(){
     thing.handle();
-    if(recv){
-        analogWrite(LED_RED, 0);
-        thing.stream();
-        if(!recvstate){ // The sender's button is pressed
-            tone(SPEAKER, PITCH);
-            analogWrite(LED_GRN, 0);
-            
-        }else{
-            noTone(SPEAKER);
-            analogWrite(LED_GRN, 1023);
-        }
-        return;
-    }
-
-    analogWrite(LED_RED, 1023);
-    int button = digitalRead(BUTTON);
-    if(button == LOW){  // The button is pressed; state inverted due to pullup pin
-        tone(SPEAKER, PITCH);
-        // Due to the RGB led being common anode, 0 turns it fully on and 1023 turns it fully off
-        analogWrite(LED_GRN, 0);
-        
-        if(!stream){
-            current_time = millis();
-            if(current_time - start_time >= 2500){
-                stream = true;
-                thing.call_device(recvname, "torecv");
-                start_time = millis();
-            }
-        }else{
-            start_time = millis();
-        }
-    }else{
-        noTone(SPEAKER);
-        analogWrite(LED_GRN, 1023);
-
-        if(stream){
-            current_time = millis();
-            if(current_time - start_time >= 5000){
-                stream = false;
-                thing.call_device(recvname, "torecv");
-                start_time = millis();
-            }
-        }else{
-            start_time = millis();
-        }
-    }
-    if(stream){
-        thing.stream(thing["button"]);
-        analogWrite(LED_BLU, 0);
-    }else{
-        analogWrite(LED_BLU, 1023);
-    }
 }
