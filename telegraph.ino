72ee063ac1c4a727732aa8b6de2e67c4e5d9a8ff
PixelSergey <sergey.ichtchenko@gmail.com>
2020-04-30 00:47:40 +0300

    Improve timing system, which is still broken
    I don't think it is possible to make one NodeMCU call another
    in such a way that the information would arrive properly.
    Another method is probably required.

--- telegraph.ino
+++ telegraph.ino
@@ -16,7 +16,7 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-
+#include <vector>
 // The preferences file is automatically included here by the Arduino IDE.
 
 ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
@@ -25,8 +25,13 @@ int start_time;
 int end_time;
 int stop_time;
 int pulse_time;
+int silence_time;
 int last;
 int duration;
+int command;
+
+std::vector<int> play;
+std::vector<int> silence;
 
 void ICACHE_RAM_ATTR timer(){
     int button = digitalRead(BUTTON);
@@ -34,6 +39,7 @@ void ICACHE_RAM_ATTR timer(){
     while(millis() < delayt);
     if(button == LOW){
         start_time = millis();
+        if(last != LOW) silence_time = start_time - end_time;
     }else{
         end_time = millis();
         if(last != HIGH) pulse_time = end_time - start_time;
@@ -55,7 +61,9 @@ void setup(){
     analogWrite(LED_BLU, 1023);
 
     pulse_time = 0;
+    silence_time = 0;
     duration = 0;
+    command = 0;
     
     attachInterrupt(digitalPinToInterrupt(BUTTON), timer, CHANGE);
 
@@ -65,9 +73,15 @@ void setup(){
 
     thing.add_wifi(WIFI_SSID, WIFI_PASSWORD);
     
-    thing["play"] << [](pson& in){
-        duration = (int) in["duration"];
-        Serial.println(duration);
+    thing["command"] << [](pson& in){
+        int play_t = (int) in["play"];
+        int silence_t = (int) in["silence"];
+        if(play_t != 0){
+            play.push_back(play_t);
+        }
+        if(silence_t != 0){
+            silence.push_back(silence_t);
+        }
     };
 }
 
@@ -75,20 +89,44 @@ void loop(){
     thing.handle();
     if(pulse_time != 0){
         pson data;
-        data["duration"] = pulse_time;
+        data["play"] = pulse_time;
         Serial.println(pulse_time);
         pulse_time = 0;
-        thing.call_device(recvname, "play", data);
+        thing.call_device(recvname, "command", data);
+    }
+    if(silence_time != 0){
+        pson data;
+        data["silence"] = silence_time;
+        Serial.println(silence_time);
+        silence_time = 0;
+        thing.call_device(recvname, "command", data);
     }
 
+    if(duration == 0 && (!play.empty() || !silence.empty())){
+        if(play.size() >= silence.size()){
+            duration = play.front();
+            play.erase(play.begin());
+            command = 1;
+        }else{
+            duration = silence.front();
+            silence.erase(silence.begin());
+            command = 0;
+        }
+    }
     if(duration != 0){
         stop_time = millis() + duration;
         duration = 0;
-        tone(SPEAKER, PITCH);
-        analogWrite(LED_GRN, 0);
+        if(command == 1){
+            tone(SPEAKER, PITCH);
+            analogWrite(LED_GRN, 0);
+        }else{
+            noTone(SPEAKER);
+            analogWrite(LED_GRN, 1023);
+        }
     }
     if(millis() > stop_time){
         noTone(SPEAKER);
         analogWrite(LED_GRN, 1023);
+        duration = 0;
     }
 }
