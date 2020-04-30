d43f9b59efe73bbb1c3faaa08182774e527c8807
PixelSergey <sergey.ichtchenko@gmail.com>
2020-05-01 00:03:37 +0300

    Change NTP synchronisation to millis() synchronisation
    It finally works. <3

--- telegraph.ino
+++ telegraph.ino
@@ -18,26 +18,28 @@
 
 // The preferences file is automatically included here by the Arduino IDE.
 
+// System variables
 ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", 0);
-
 char recvname[9]; // The name of the receiver
-unsigned long init_time;
-int current_millis;
+long dt; // The time difference between both NodeMCUs
 
-int last;
+// Output variables
 int out_command;
+int last;
 
-unsigned long in_init_time;
-std::vector<int> in_millis;
+// Input variables
+std::vector<unsigned long> in_millis;
 std::vector<int> in_command;
 
-void ICACHE_RAM_ATTR timer(){
+/**
+ * Interrupt function. Immediately interrupts the main program and
+ * jumps here when a button state change is detected.
+ * The ICACHE_RAM_ATTR flag is needed to put the function in RAM,
+ * as that is required to avoid a crash.
+ */
+void ICACHE_RAM_ATTR on_interrupt(){
     int button = digitalRead(BUTTON);
-    if(last != button){
-        out_command = button;
-    }
+    if(last != button) out_command = button;
     last = button;
 }
 
@@ -45,19 +47,20 @@ void setup(){
     Serial.begin(9600);
     pinMode(BUTTON, INPUT_PULLUP);
     pinMode(SPEAKER, OUTPUT);
-    // Due to the RGB led being common anode, 0 turns an LED fully on and 1023 turns it fully off
     pinMode(LED_RED, OUTPUT);
     pinMode(LED_GRN, OUTPUT);
     pinMode(LED_BLU, OUTPUT);
-
+    // Due to the RGB led having a common anode, analog 0 turns
+    // an LED fully on and analog 1023 turns it fully off
     analogWrite(LED_RED, 1023);
     analogWrite(LED_GRN, 1023);
     analogWrite(LED_BLU, 1023);
-
+    
     last = 0;
     out_command = -1;
-    
-    attachInterrupt(digitalPinToInterrupt(BUTTON), timer, CHANGE);
+
+    // Attach the interrupt function to any change in state of the button
+    attachInterrupt(digitalPinToInterrupt(BUTTON), on_interrupt, CHANGE);
     
     // The most cursed and hacky line of code you will ever see
     // Converts NodeMCU1 <-> NodeMCU0 to find out the recipient's name
@@ -65,60 +68,57 @@ void setup(){
     
     thing.add_wifi(WIFI_SSID, WIFI_PASSWORD);
     
-    thing.handle();
-    timeClient.begin();
-    timeClient.update();
-    current_millis = millis();
-    init_time = (unsigned long)(timeClient.getEpochTime() - current_millis/1000);
-    Serial.println();
-    Serial.printf("Init. time: %d\n", init_time);
-    Serial.printf("Millis. time: %d\n", current_millis);
-    timeClient.end();
-    
     thing["command"] << [](pson& in){
-        in_init_time = (unsigned long) in["inittime"];
-        Serial.printf("Init Time: %d\n", in_init_time);
         in_command.push_back((int) in["command"]);
-        Serial.printf("Command: %d\n", in_command.front());
-        in_millis.push_back((int) in["millis"]);
-        Serial.printf("Millis: %d\n", in_millis.front());
+        //Serial.printf("Command: %d\n", in_command.front());
+        in_millis.push_back((unsigned long) in["millis"]);
+        //Serial.printf("Millis: %d\n", in_millis.front());
     };
+    thing["timesync"] << [](pson& in){
+        dt = millis() - (unsigned long) in["millis"];
+        Serial.printf("Calculated dt = %d", dt);
+        pson data;
+        data["dt"] = -dt;
+        thing.call_device(recvname, "timeresponse", data);
+    };
+    thing["timeresponse"] << [](pson& in){
+        dt = (long) in["dt"];
+        Serial.printf("Received dt = %d", dt);
+    };
+
+    // Attempt to call the other NodeMCU right off the bat.
+    // The first one to boot will fail, the second will succeed.
+    thing.handle();
+    pson data;
+    data["millis"] = millis();
+    thing.call_device(recvname, "timesync", data);
 }
 
 void loop(){
     thing.handle();
-    current_millis = millis();
     
     if(out_command != -1){
-        pson data;
-        data["inittime"] = init_time;
-        data["command"] = out_command;
-        data["millis"] = current_millis;
+        pson payload;
+        payload["command"] = out_command;
+        payload["millis"] = millis();
         Serial.println(out_command);
         out_command = -1;
-        thing.call_device(recvname, "command", data);
+        thing.call_device(recvname, "command", payload);
     }
     if(!in_command.empty() && !in_millis.empty()){
-        // Check the whole number of seconds first
-        unsigned long start_sec = in_init_time + (unsigned long)(in_millis.front()/1000);
-        unsigned long now_sec = init_time + (unsigned long)(current_millis/1000);
-        if(start_sec >= now_sec){
-            // And then check the millisecond part
-            int start_ms = in_millis.front() - 1000*(int)(in_millis.front()/1000);
-            int now_ms = current_millis - 1000*(int)(current_millis/1000);
-            if(start_ms >= now_ms){
-                if(in_command.front() == LOW){
-                    Serial.println("Activated");
-                    analogWrite(LED_GRN, 0);
-                    tone(SPEAKER, PITCH);
-                }else{
-                    Serial.println("De-activated");
-                    analogWrite(LED_GRN, 1023);
-                    noTone(SPEAKER);
-                }
-                in_command.erase(in_command.begin());
-                in_millis.erase(in_millis.begin());
+        if(millis() > in_millis.front() + dt + 1000){
+            // The button emits a LOW voltage when pressed due to the INPUT_PULLUP mode
+            if(in_command.front() == LOW){
+                analogWrite(LED_GRN, 0);
+                tone(SPEAKER, PITCH);
+                Serial.println("Activated");
+            }else{
+                analogWrite(LED_GRN, 1023);
+                noTone(SPEAKER);
+                Serial.println("De-activated");
             }
+            in_command.erase(in_command.begin());
+            in_millis.erase(in_millis.begin());
         }
     }
 }
